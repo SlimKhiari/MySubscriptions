fdd670d38df98ad96b8d0c0719ed196d1c5f215e
SlimKhiari <slim.khiari@ens.uvsq.fr>
2024-06-18 15:12:25 +0200

    add

--- client/src/Users.jsx
+++ client/src/Users.jsx
@@ -1,17 +1,24 @@
-import React,{ useState } from "react";
+import React,{ useState, useEffect} from "react";
 import { Link } from "react-router-dom";
+import axios from 'axios'
+
 
 function Users () {
-    const [users, setUsers] = useState([{
-        Name: "slim", Email: "slim.khiari.03@gmail.com", Age: 25
-    }])
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        axios.get("http://localhost:3001")
+        .then(result => setUsers(result.data))
+        .catch(err => console.log(err))
+    }, [])
+
     return (
         <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
             <div className='w-50 bg-white rounded p-3'>
                   <Link to="/create" className='btn btn-success'>Add +</Link>
                   <table className='table'>
                         <thead>
-                            <tr>
+                            <tr>  
                                 <th>Name</th>
                                 <th>Email</th>
                                 <th>Age</th>
@@ -20,14 +27,14 @@ function Users () {
                         </thead>
                         <tbody>
                            {
-                                users.map((user) => {
+                                users.map((users) => {
                                    return <tr>
-                                        <td>{user.Name}</td>
-                                        <td>{user.Email}</td>
-                                        <td>{user.Age}</td>
+                                        <td>{users.name}</td>
+                                        <td>{users.email}</td>
+                                        <td>{users.age}</td>
                                         <td>
                                             <Link to="/update" className='btn btn-success'>Update</Link>
-                                            <button>Delete</button>
+                                            <button className='btn btn-danger'>Delete</button>
                                         </td>
                                     </tr>
                                 })
